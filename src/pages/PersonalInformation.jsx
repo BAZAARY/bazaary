b6b11adc8a34fd2d3c873f02d4e583d3e94a0c0a
David Moreno <davidan7845@gmail.com>
2023-10-27 17:49:04 -0500

    se completo la interfaz que muestra la informacion del usuario

--- src/pages/PersonalInformation.jsx
+++ src/pages/PersonalInformation.jsx
@@ -5,6 +5,10 @@ import InformationText from "../components/personalInformation/informationItem";
 
 const PersonalInformation = () => {
 
+	const navigate = useNavigate(); // Hook de navegación
+
+	const iconPath = `${process.env.PUBLIC_URL}/edit.svg`;
+
 	const user = {
 		name: 'David',
 		email: 'david@david.com',
@@ -42,71 +46,83 @@ const PersonalInformation = () => {
 
 	return (
 		<div className="flex flex-col items-center">
-				<p className="flex self-start mb-4 mt-8 ml-8">Volver</p>
-				<h2 className="mb-6">Mi Información</h2>
+			<button onClick={() => navigate("/profile")} className={` flex self-start justify-center mt-8 ml-6 bg-[#FD9200] hover:bg-hover-color-button active:bg-click-color-button text-white font-bold py-2 px-4 rounded-xl text-xl border border-blue-text-button h-12 shadow-sm w-28 shadow-purple-text `}>
+				Volver
+			</button>
+			<h2 className="mb-4 text-2xl font-bold">Mi Información</h2>
+
 			{editing ? (
-				<div>
-					<input
-						type="text"
-						name="name"
-						value={userInfo.name}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="email"
-						name="email"
-						value={userInfo.email}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="text"
-						name="userName"
-						value={userInfo.username}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="text"
-						name="cedula"
-						value={userInfo.cedula}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="tel"
-						name="phoneNumber1"
-						value={userInfo.phoneNumber1}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="tel"
-						name="phoneNumber2"
-						value={userInfo.phoneNumber2}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="text"
-						name="address1"
-						value={userInfo.address1}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
-					<input
-						type="text"
-						name="address2"
-						value={userInfo.address2}
-						onChange={handleInputChange}
-						className="border-none"
-					/>
+				<>
+					<div>
+						<input
+							type="text"
+							name="name"
+							value={userInfo.name}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="email"
+							name="email"
+							value={userInfo.email}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="text"
+							name="userName"
+							value={userInfo.username}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="text"
+							name="cedula"
+							value={userInfo.cedula}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="tel"
+							name="phoneNumber1"
+							value={userInfo.phoneNumber1}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="tel"
+							name="phoneNumber2"
+							value={userInfo.phoneNumber2}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="text"
+							name="address1"
+							value={userInfo.address1}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+						<input
+							type="text"
+							name="address2"
+							value={userInfo.address2}
+							onChange={handleInputChange}
+							className="border-none"
+						/>
+					</div>
 					<button onClick={handleSaveClick}>Guardar</button>
 					<button onClick={handleCancelClick}>Cancelar</button>
-				</div>
+				</>
+
 			) : (
 				<div >
+					<button
+						className="text-white font-bold py-2 px-4 rounded ml-96"
+						onClick={handleEditClick}
+					>
+						<img src={iconPath} alt="Icono" className="w-4 h-4 " />
+					</button>
 					<InformationText title={"Nombre : "} text={userInfo.name} />
 					<InformationText title={"Email : "} text={userInfo.email} />
 					<InformationText title={"Cedula : "} text={userInfo.cedula} />
@@ -114,7 +130,6 @@ const PersonalInformation = () => {
 					<InformationText title={"Telefono 2 : "} text={userInfo.phoneNumber2} />
 					<InformationText title={"Dirección de envío 1 : "} text={userInfo.address1} />
 					<InformationText title={"Dirección de envío 2 : "} text={userInfo.address2} />
-					<button onClick={handleEditClick}>Editar</button>
 				</div>
 			)}
 		</div>
