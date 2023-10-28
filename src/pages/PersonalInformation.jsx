70e6a73978098fd22601e8a1a509364cc2b8ef4f
David Moreno <davidan7845@gmail.com>
2023-10-28 13:21:18 -0500

    frontend de edicion de informacion personal

--- src/pages/PersonalInformation.jsx
+++ src/pages/PersonalInformation.jsx
@@ -44,75 +44,124 @@ const PersonalInformation = () => {
 		setUserInfo({ ...userInfo, [name]: value });
 	};
 
+	const handleBackButton = () => {
+		if (editing) {
+			handleCancelClick();
+		}
+		else {
+			navigate("/profile")
+		}
+	}
+
 	return (
 		<div className="flex flex-col items-center">
-			<button onClick={() => navigate("/profile")} className={` flex self-start justify-center mt-8 ml-6 bg-[#FD9200] hover:bg-hover-color-button active:bg-click-color-button text-white font-bold py-2 px-4 rounded-xl text-xl border border-blue-text-button h-12 shadow-sm w-28 shadow-purple-text `}>
+			<button onClick={handleBackButton} className={` flex self-start justify-center mt-8 ml-6 bg-[#FD9200] hover:bg-hover-color-button active:bg-click-color-button text-white font-bold py-2 px-4 rounded-xl text-xl border border-blue-text-button h-12 shadow-sm w-28 shadow-purple-text `}>
 				Volver
 			</button>
 			<h2 className="mb-4 text-2xl font-bold">Mi Información</h2>
 
 			{editing ? (
 				<>
-					<div>
-						<input
-							type="text"
-							name="name"
-							value={userInfo.name}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="email"
-							name="email"
-							value={userInfo.email}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="text"
-							name="userName"
-							value={userInfo.username}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="text"
-							name="cedula"
-							value={userInfo.cedula}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="tel"
-							name="phoneNumber1"
-							value={userInfo.phoneNumber1}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="tel"
-							name="phoneNumber2"
-							value={userInfo.phoneNumber2}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="text"
-							name="address1"
-							value={userInfo.address1}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
-						<input
-							type="text"
-							name="address2"
-							value={userInfo.address2}
-							onChange={handleInputChange}
-							className="border-none"
-						/>
+					<div className="flex justify-center flex-wrap w-[40rem]">
+						<div className="flex flex-col w-1/2 items-start mb-8">
+							<label htmlFor="name" className="mb-2 flex ">Nombre:</label>
+							<input
+								type="text"
+								name="name"
+								value={userInfo.name}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start pl-28">
+							<label htmlFor="email" className="mb-2">Correo electrónico:</label>
+							<input
+								type="email"
+								name="email"
+								value={userInfo.email}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start mb-8">
+							<label htmlFor="userName" >Nombre de usuario:</label>
+							<input
+								type="text"
+								name="userName"
+								value={userInfo.username}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start pl-28">
+							<label htmlFor="cedula" >Cedula:</label>
+							<input
+								type="text"
+								name="cedula"
+								value={userInfo.cedula}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start mb-8">
+							<label htmlFor="phoneNumber1" >Teléfono:</label>
+							<input
+								type="tel"
+								name="phoneNumber1"
+								value={userInfo.phoneNumber1}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start pl-28">
+							<label htmlFor="phoneNumber2" >Segundo Teléfono:</label>
+							<input
+								type="tel"
+								name="phoneNumber2"
+								value={userInfo.phoneNumber2}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start">
+							<label htmlFor="address1" >Dirección:</label>
+							<input
+								type="text"
+								name="address1"
+								value={userInfo.address1}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
+						<div className="flex flex-col w-1/2 items-start pl-28">
+							<label htmlFor="address2" >Segunda Dirección:</label>
+							<input
+								type="text"
+								name="address2"
+								value={userInfo.address2}
+								onChange={handleInputChange}
+								className="rounded-md"
+							/>
+						</div>
+
 					</div>
-					<button onClick={handleSaveClick}>Guardar</button>
-					<button onClick={handleCancelClick}>Cancelar</button>
+
+					<div className="flex mb-8 justify-center">
+						<button onClick={handleSaveClick} className={` flex self-start justify-center mt-8 ml-6 bg-green-500  text-white font-bold py-2 px-4 rounded-xl text-xl border border-blue-text-button h-12 shadow-sm w-28 shadow-purple-text `}>
+							Guardar
+						</button>
+
+						<button onClick={handleCancelClick} className={` flex self-start justify-center mt-8 ml-6 bg-red-500 text-white font-bold py-2 px-4 rounded-xl text-xl border border-blue-text-button h-12 shadow-sm w-28 shadow-purple-text `}>
+							Cancelar
+						</button>
+					</div>
 				</>
 
 			) : (
